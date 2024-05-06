7534a438849876f3f22fda169cd0859e40b2197a
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2024-05-06 11:13:36 +0700

    f

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:fashion_ecom_app/constants/size_config.dart';
 import 'package:fashion_ecom_app/constants/style.dart';
 import 'package:fashion_ecom_app/presentation/widgets/image_network_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -25,56 +26,98 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Stack(
-          children: [
-            CarouselSlider.builder(
-              carouselController: _carouselController,
-              itemCount: images.length,
-              itemBuilder: (context, index, realIndex) {
-                return ImageNetWorkWidget(
-                  imageUrl: images[index],
+        _buildSlideShow(),
+        MasonryGridView.count(
+          padding: EdgeInsets.all(scaleFontSize(appSpace)),
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: 4,
+          crossAxisCount: 2,
+          crossAxisSpacing: scaleFontSize(appSpace),
+          mainAxisSpacing: scaleFontSize(appSpace),
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                ImageNetWorkWidget(
+                  fit: BoxFit.cover,
+                  imageUrl:
+                      'https://img.freepik.com/free-photo/black-woman-trendy-grey-leather-jacket-posing-beige-background-studio-winter-autumn-fashion-look_273443-141.jpg',
                   width: double.infinity,
-                  height: scaleFontSize(300),
+                  height: scaleFontSize(200),
                   isNotRounding: true,
-                );
-              },
-              options: CarouselOptions(
-                autoPlay: true,
-                viewportFraction: 1,
-                aspectRatio: 2.0,
-                enlargeCenterPage: true,
-                enlargeStrategy: CenterPageEnlargeStrategy.height,
-                height: scaleFontSize(300),
-                autoPlayAnimationDuration: const Duration(seconds: 1),
-                autoPlayInterval: const Duration(seconds: 10),
-                enlargeFactor: 0,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
-            ),
-            Positioned(
-              bottom: scaleFontSize(appSpace),
-              right: 0,
-              left: 0,
-              child: Center(
-                child: AnimatedSmoothIndicator(
-                  activeIndex: _currentIndex,
-                  count: images.length,
-                  textDirection: TextDirection.rtl,
-                  effect: WormEffect(
-                    type: WormType.thinUnderground,
-                    dotWidth: scaleFontSize(14),
-                    dotHeight: scaleFontSize(14),
-                    dotColor: backgroundColor,
-                    activeDotColor: secondaryColor,
-                  ),
                 ),
+                SizedBox(height: scaleFontSize(5)),
+                Text(
+                  '21WN reversible angora cardigan',
+                  style: TextStyle(
+                    fontSize: scaleFontSize(16),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  '\$120',
+                  style: TextStyle(
+                      fontSize: scaleFontSize(24),
+                      color: Theme.of(context).primaryColor),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Stack _buildSlideShow() {
+    return Stack(
+      children: [
+        CarouselSlider.builder(
+          carouselController: _carouselController,
+          itemCount: images.length,
+          itemBuilder: (context, index, realIndex) {
+            return ImageNetWorkWidget(
+              imageUrl: images[index],
+              width: double.infinity,
+              height: scaleFontSize(300),
+              isNotRounding: true,
+            );
+          },
+          options: CarouselOptions(
+            autoPlay: true,
+            viewportFraction: 1,
+            aspectRatio: 2.0,
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            height: scaleFontSize(300),
+            autoPlayAnimationDuration: const Duration(seconds: 1),
+            autoPlayInterval: const Duration(seconds: 10),
+            enlargeFactor: 0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ),
+        Positioned(
+          bottom: scaleFontSize(appSpace),
+          right: 0,
+          left: 0,
+          child: Center(
+            child: AnimatedSmoothIndicator(
+              activeIndex: _currentIndex,
+              count: images.length,
+              textDirection: TextDirection.rtl,
+              effect: WormEffect(
+                type: WormType.thinUnderground,
+                dotWidth: scaleFontSize(14),
+                dotHeight: scaleFontSize(14),
+                dotColor: backgroundColor,
+                activeDotColor: secondaryColor,
               ),
             ),
-          ],
+          ),
         ),
       ],
     );
