d3a25a0e07109cc80e9022374dc302a1f34790b3
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2024-05-07 13:51:15 +0700

    add

---
+++ lib/presentation/screens/item_filter/item_filter_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemFilterScreen extends StatefulWidget {
+  const ItemFilterScreen({super.key});
+
+  @override
+  State<ItemFilterScreen> createState() => _ItemFilterScreenState();
+}
+
+class _ItemFilterScreenState extends State<ItemFilterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
